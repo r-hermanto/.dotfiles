0956575d7486c4c400f434a69dbb6a4d6353e956
Rudi Hermanto <68783679+r-hermanto@users.noreply.github.com>
2024-01-04 12:36:53 +0700

    nvim: Add shortcut git blame

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -261,7 +261,13 @@ require('nvim-treesitter.configs').setup({
 
 require('Comment').setup()
 
-require('gitsigns').setup()
+require('gitsigns').setup({
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local opts = { buffer = bufnr }
+        vim.keymap.set('n', '<leader>bl', gs.blame_line, opts)
+    end
+})
 
 require('ibl').setup()
 
@@ -274,7 +280,7 @@ vim.keymap.set('n', '<leader>ds', dap.continue, { desc = 'Debug: Start/Continue'
 vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'Debug: Step Into' })
 vim.keymap.set('n', '<leader>dn', dap.step_over, { desc = 'Debug: Step Over' })
 vim.keymap.set('n', '<leader>do', dap.step_out, { desc = 'Debug: Step Out' })
-vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+vim.keymap.set('n', '<leader>bp', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
 vim.keymap.set('n', '<leader>B', function()
     dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
 end, { desc = 'Debug: Set Breakpoint' })
