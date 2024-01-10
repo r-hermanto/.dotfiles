99cfbeeec3ada2f47eb8b3251b1e5c377119d4d8
Rudi Hermanto <68783679+r-hermanto@users.noreply.github.com>
2024-01-10 22:34:08 +0700

    nvim: move debugger plugin to separate file

--- nvim/.config/nvim/lua/pkg_manager/plugins/init.lua
+++ nvim/.config/nvim/lua/pkg_manager/plugins/init.lua
@@ -1,13 +1,5 @@
 return {
-    { "numToStr/Comment.nvim",  config = true },
+    { "numToStr/Comment.nvim",               config = true },
     { "sindrets/diffview.nvim" },
-    { "lukas-reineke/indent-blankline.nvim",    main = "ibl",       config = true },
-    {
-        "mfussenegger/nvim-dap",
-        dependencies = {
-            "rcarriga/nvim-dap-ui",
-            "williamboman/mason.nvim",
-            "leoluz/nvim-dap-go",
-        }
-    },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", config = true },
 }
