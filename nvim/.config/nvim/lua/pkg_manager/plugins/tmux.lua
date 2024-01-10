e49de4aa8ae87b7b4d8875f76d2ba2d7df691e9d
Rudi Hermanto <68783679+r-hermanto@users.noreply.github.com>
2024-01-11 02:53:47 +0700

    nvim: tmux navigator keymap

--- nvim/.config/nvim/lua/pkg_manager/plugins/tmux.lua
+++ nvim/.config/nvim/lua/pkg_manager/plugins/tmux.lua
@@ -1,11 +1,10 @@
 return {
     "numToStr/Navigator.nvim",
     config = true,
-    keymaps = {
-        vim.keymap.set({ "n", "t" }, "<M-h>", ":NavigatorLeft<CR>"),
-        vim.keymap.set({ "n", "t" }, "<M-l>", ":NavigatorRight<CR>"),
-        vim.keymap.set({ "n", "t" }, "<M-k>", ":NavigatorUp<CR>"),
-        vim.keymap.set({ "n", "t" }, "<M-j>", ":NavigatorDown<CR>"),
-        vim.keymap.set({ "n", "t" }, "<M-p>", ":NavigatorPrevious<CR>"),
+    keys = {
+        { "<M-h>", ":NavigatorLeft<CR>" },
+        { "<M-l>", ":NavigatorRight<CR>" },
+        { "<M-k>", ":NavigatorUp<CR>" },
+        { "<M-j>", ":NavigatorDown<CR>" },
     }
 }
