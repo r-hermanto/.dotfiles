0460d45c4eb9890823515aa46d4e51d52dbf2604
Rudi Hermanto <68783679+r-hermanto@users.noreply.github.com>
2024-01-11 01:50:03 +0700

    nvim: Reenable luasnip

--- nvim/.config/nvim/lua/pkg_manager/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/pkg_manager/plugins/nvim-cmp.lua
@@ -1,7 +1,7 @@
 return {
     "hrsh7th/nvim-cmp",
     dependencies = {
-        -- "L3MON4D3/LuaSnip",
+        "L3MON4D3/LuaSnip",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-nvim-lsp-signature-help"
     },
@@ -9,11 +9,11 @@ return {
     config = function()
         local cmp = require("cmp")
         cmp.setup({
-            -- snippet = {
-            --     expand = function(args)
-            --         require("luasnip").lsp_expand(args.body)
-            --     end
-            -- },
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end
+            },
             mapping = cmp.mapping.preset.insert {
                 ["<C-n>"] = cmp.mapping.select_next_item(),
                 ["<C-p>"] = cmp.mapping.select_prev_item(),
