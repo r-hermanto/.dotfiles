2b2aa28896c7e79e335004b97873bf268d5e44be
Rudi Hermanto <68783679+r-hermanto@users.noreply.github.com>
2024-04-28 12:22:15 +0700

    nvim: use eslint linter instead of daemon one
    somehow eslint daemon not works so use normal one instead. For now it's fast enough so not a biggie

--- nvim/.config/nvim/lua/pkg_manager/plugins/linter.lua
+++ nvim/.config/nvim/lua/pkg_manager/plugins/linter.lua
@@ -5,13 +5,13 @@ return {
         local lint = require("lint")
 
         lint.linters_by_ft = {
-            javascript = { "eslint_d" },
-            typescript = { "eslint_d" },
+            javascript = { "eslint" },
+            typescript = { "eslint" },
         }
 
         vim.api.nvim_create_autocmd({ "BufWritePost" }, {
             callback = function()
-                require("lint").try_lint()
+                lint.try_lint()
             end,
         })
     end
