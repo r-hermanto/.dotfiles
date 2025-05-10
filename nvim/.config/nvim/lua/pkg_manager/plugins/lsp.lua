e592ff933ccd199129f686639aa1c78d6c76da06
Rudi Hermanto <68783679+r-hermanto@users.noreply.github.com>
2025-05-10 16:03:17 +0700

    nvim: mason lspconfig breaking changes

--- nvim/.config/nvim/lua/pkg_manager/plugins/lsp.lua
+++ nvim/.config/nvim/lua/pkg_manager/plugins/lsp.lua
@@ -9,38 +9,6 @@ return {
     config = function()
         local mason = require("mason")
         local mason_lspconfig = require("mason-lspconfig")
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-        local on_attach = function(_, bufnr)
-            local telescope_builtin = require("telescope.builtin")
-            local opts = { buffer = bufnr }
-
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            vim.keymap.set("n", "gi", telescope_builtin.lsp_implementations, opts)
-            vim.keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, opts)
-            vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-            vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-            vim.keymap.set("n", "<space>wl", function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, opts)
-            vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-            vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-            vim.keymap.set("n", "gr", telescope_builtin.lsp_references, opts)
-            vim.keymap.set("n", "<leader>sc", telescope_builtin.quickfix, opts)
-            vim.keymap.set("n", "<leader>ss", telescope_builtin.lsp_dynamic_workspace_symbols, opts)
-
-            vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-            vim.keymap.set("n", "[d", function()
-                vim.diagnostic.jump({ count = -1, float = true })
-            end, opts)
-            vim.keymap.set("n", "]d", function()
-                vim.diagnostic.jump({ count = 1, float = true })
-            end, opts)
-        end
 
         local servers = {
             gopls = {},
@@ -55,18 +23,64 @@ return {
 
         mason.setup()
         mason_lspconfig.setup({
-            automatic_installation = false,
+            automatic_enable = true,
             ensure_installed = vim.tbl_keys(servers)
         })
 
-        mason_lspconfig.setup_handlers {
-            function(server_name)
-                require("lspconfig")[server_name].setup({
-                    capabilities = capabilities,
-                    settings = servers[server_name],
-                    on_attach = on_attach,
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+            callback = function(args)
+                local telescope_builtin = require("telescope.builtin")
+                local opts = { buffer = args.buf }
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "gi", telescope_builtin.lsp_implementations, opts)
+                vim.keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, opts)
+                vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+                vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+                vim.keymap.set("n", "<space>wl", function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, opts)
+                vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+                vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "gr", telescope_builtin.lsp_references, opts)
+                vim.keymap.set("n", "<leader>sc", telescope_builtin.quickfix, opts)
+                vim.keymap.set("n", "<leader>ss", telescope_builtin.lsp_dynamic_workspace_symbols, opts)
+
+                vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+                vim.keymap.set("n", "[d", function()
+                    vim.diagnostic.jump({ count = -1, float = true })
+                end, opts)
+                vim.keymap.set("n", "]d", function()
+                    vim.diagnostic.jump({ count = 1, float = true })
+                end, opts)
+
+
+                local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+                vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                    buffer = args.buf,
+                    group = highlight_augroup,
+                    callback = vim.lsp.buf.document_highlight,
                 })
+
+                vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                    buffer = args.buf,
+                    group = highlight_augroup,
+                    callback = vim.lsp.buf.clear_references,
+                })
+
+                vim.api.nvim_create_autocmd('LspDetach', {
+                    group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+                    callback = function(event2)
+                        vim.lsp.buf.clear_references()
+                        vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+                    end,
+                })
+
+                vim.lsp.inlay_hint.enable(true)
             end
-        }
+        })
     end
 }
