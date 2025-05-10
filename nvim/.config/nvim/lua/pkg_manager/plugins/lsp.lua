5dc9666d2feee78a83389b379184d64c62fef069
Rudi Hermanto <68783679+r-hermanto@users.noreply.github.com>
2025-05-10 16:08:01 +0700

    nvim: replace neodev with lazydev

--- nvim/.config/nvim/lua/pkg_manager/plugins/lsp.lua
+++ nvim/.config/nvim/lua/pkg_manager/plugins/lsp.lua
@@ -3,7 +3,17 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        { "folke/neodev.nvim", opts = {} },
+        {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    -- See the configuration section for more details
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
     },
     event = { "BufReadPre", "BufNewFile" },
     config = function()
